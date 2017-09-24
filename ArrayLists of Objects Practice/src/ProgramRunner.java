bcf80376d73164d22506a2abaeeaf6b278e02adc
ethanwalters <ethanwalters@192.168.10.77>
2017-09-24 12:58:26 -0600

    nice

--- ArrayLists of Objects Practice/src/ProgramRunner.java
+++ ArrayLists of Objects Practice/src/ProgramRunner.java
@@ -1,16 +1,27 @@
 import java.util.ArrayList;
-
 public class ProgramRunner
 	{
+		static ArrayList <Project> TVShows = new ArrayList <Project> ( );
+		
 		public static void main(String[] args)
 			{
-				ArrayList <Project> TVShows = new ArrayList <Project> ( );
+				
 				
 				TVShows.add(new Project("South Park" , "Comedy" , 20));
 				TVShows.add(new Project("Simpsons" , "Comedy" , 28));
 				TVShows.add(new Project("Famly guy" , "Comedy" , 15));
 				TVShows.add(new Project("Grey's Anatomy" , "Drama" , 13));
 				TVShows.add(new Project("Futurama" , "Comedy" , 7));
+				
+				
+				printList();
 		
 			}
+		public static void printList(){
+			
+			for(Project p : TVShows){
+			System.out.println(p.getGenre());
+			
+			}
+		}
 	}
